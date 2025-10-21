5516d19809d85688b986e08c2bf9fe845563e5b3
byteguy8 <byteguy8@gmail.com>
2025-10-21 16:10:12 -0400

    code refactor

--- lzarena.h
+++ lzarena.h
@@ -61,9 +61,20 @@ void lzregion_destroy(LZRegion *region);
 
 size_t lzregion_available(LZRegion *region);
 size_t lzregion_available_alignment(size_t alignment, LZRegion *region);
-void *lzregion_alloc_align(size_t size, size_t alignment, LZRegion *region, size_t *out_bytes);
+void *lzregion_alloc_align(
+	size_t size,
+	size_t alignment,
+	LZRegion *region,
+	size_t *out_bytes
+);
 void *lzregion_calloc_align(size_t size, size_t alignment, LZRegion *region);
-void *lzregion_realloc_align(void *ptr, size_t old_size, size_t new_size, size_t alignment, LZRegion *region);
+void *lzregion_realloc_align(
+	void *ptr,
+	size_t old_size,
+	size_t new_size,
+	size_t alignment,
+	LZRegion *region
+);
 
 LZArena *lzarena_create(LZArenaAllocator *allocator);
 void lzarena_destroy(LZArena *arena);
@@ -75,9 +86,17 @@ void lzarena_free_all(LZArena *arena);
 
 void *lzarena_alloc_align(size_t size, size_t alignment, LZArena *arena);
 void *lzarena_calloc_align(size_t size, size_t alignment, LZArena *arena);
-void *lzarena_realloc_align(void *ptr, size_t old_size, size_t new_size, size_t alignment, LZArena *arena);
+void *lzarena_realloc_align(
+	void *ptr,
+	size_t old_size,
+	size_t new_size,
+	size_t alignment,
+	LZArena *arena
+);
 
-#define LZARENA_ALLOC(_size, _arena)(lzarena_alloc_align(_size, LZARENA_DEFAULT_ALIGNMENT, _arena))
-#define LZARENA_REALLOC(_ptr, _old_size, _new_size, _arena)(lzarena_realloc_align(_ptr, _old_size, _new_size, LZARENA_DEFAULT_ALIGNMENT, _arena))
+#define LZARENA_ALLOC(_size, _arena) \
+    (lzarena_alloc_align(_size, LZARENA_DEFAULT_ALIGNMENT, _arena))
+#define LZARENA_REALLOC(_ptr, _old_size, _new_size, _arena) \
+ (lzarena_realloc_align(_ptr, _old_size, _new_size, LZARENA_DEFAULT_ALIGNMENT, _arena))
 
 #endif
